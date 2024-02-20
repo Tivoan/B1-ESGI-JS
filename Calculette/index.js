f8621e0aa7e5804ede65c4f6fb4fd026a4c258e0
Tivoan <jeanclement.ta@gmail.com>
2024-02-20 13:01:05 +0100

    calc 2

--- Calculette/index.js
+++ Calculette/index.js
@@ -1,4 +1,27 @@
-function afficher(val) {
-    document.getElementById("finit").value += val;
-}
+const form = document.getElementById("form")
+const nbr1 = document.getElementById("nbr1")
+const nbr2 = document.getElementById("nbr2")
+const select = document.getElementById("select")
+const result = document.getElementById("result")
 
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+}
+console.log("valeur de l'élement selectionné", nbr1.value)
+console.log("valeur de l'élement selectionné", nbr2.value)
+
+switch (select.value){
+    case "add":
+    result.innerText = Number(nbr1.value)+Number(nbr2.value)
+    break;
+    case "sub":
+    result.innerText = Number(nbr1.value)-Number(nbr2.value)
+    break;
+    case "mul":
+    result.innerText = Number(nbr1.value)*Number(nbr2.value)
+    break;
+    case "div":
+    result.innerText = Number(nbr1.value)/Number(nbr2.value)
+    break;
+}
