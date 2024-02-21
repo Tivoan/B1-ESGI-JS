f2978de907945b1b835d3bcc75a2288ec0d8b851
Tivoan <jeanclement.ta@gmail.com>
2024-02-21 12:51:44 +0100

    Finito le Kart

--- Shopping List/Index.js
+++ Shopping List/Index.js
@@ -1,6 +1,24 @@
 function Ajout(NewFruit){
     Basket.push(NewFruit)
-    document.getElementById("result").innerHTML = Basket ;
+    document.getElementById("result").innerHTML = Basket + "<br>" ;
+
+    switch(NewFruit){
+        case 'Banana':
+            NbrBanane = NbrBanane + 1
+            break
+        case 'Strawberry':
+            NbrFraise = NbrFraise + 1
+            break
+        case 'Pear' : 
+            NbrPoire = NbrPoire + 1
+            break
+
+    }
+    document.getElementById("resultCom").innerHTML = "Banane = " + NbrBanane + "<br>" + "Fraise = " + NbrFraise + "<br>" + "Poire = " + NbrPoire + "<br>" ;
+   
 }
 
 let Basket = []
+let NbrBanane = 0
+let NbrFraise = 0
+let NbrPoire = 0
