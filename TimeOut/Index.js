cfd7816b0bfa8dad4913405d89b0fc674e64221c
Tivoan <jeanclement.ta@gmail.com>
2024-02-29 09:54:27 +0100

    29022024

--- TimeOut/Index.js
+++
@@ -1,8 +0,0 @@
-let promesse = new Promise((Resolve,Reject) => {
-    let condition = true
-    if (condition){
-        setTimeout(() => resolve("Opération Réussie"),1000) }
-        else {
-            reject("Opération échouée")
-        }
-    })
